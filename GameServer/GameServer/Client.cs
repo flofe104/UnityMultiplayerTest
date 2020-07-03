ffe8dce0c6425f30cf1851c79d870ea0177c1367
JanOltmanns <47364947+JanOltmanns@users.noreply.github.com>
2020-07-03 21:32:24 +0200

    youtube tutorial und so
    https://www.youtube.com/watch?v=uh8XaC0Y5MA&list=PLXkn83W0QkfnqsK8I0RAz5AbUxfg3bOQ5

---
+++ GameServer/GameServer/Client.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace GameServer
+{
+    class Client
+    {
+        public static int dataBufferSize = 4096;
+
+        public int id;
+        public TCP tcp;
+
+        public Client(int _clientId)
+        {
+            id = _clientId;
+            tcp = new TCP(id);
+        }
+
+
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private readonly int id;
+            private NetworkStream stream;
+            private byte[] receiveBuffer;
+
+            public TCP(int id)
+            {
+                this.id = id;
+            }
+
+
+            public void Connect(TcpClient socket)
+            {
+                this.socket = socket;
+                this.socket.ReceiveBufferSize = dataBufferSize;
+                this.socket.SendBufferSize = dataBufferSize;
+
+                stream = this.socket.GetStream();
+
+                receiveBuffer = new byte[dataBufferSize];
+
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+
+            }
+
+
+            private void ReceiveCallback(IAsyncResult result)
+            {
+                try
+                {
+                    int byteLength = stream.EndRead(result);
+                    if (byteLength <= 0)
+                    {
+                        return;
+                    }
+
+                    byte[] data = new byte[byteLength];
+                    Array.Copy(receiveBuffer, data, byteLength);
+
+                    stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+                }
+                catch (Exception _ex)
+                {
+                    Console.WriteLine($"Error receiving TCP data: {_ex}");
+                }
+            }
+        }
+    }
+}
---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "GameServer";
+
+            Server.Start(50, 24680);
+            Console.ReadKey();
+        }
+    }
+}
